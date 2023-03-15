84d2b28b4692cc4aa6baf73a75300a2cac56815b
satendra-patel <patelsatendra06@gmail.com>
2023-03-15 23:28:07 +0530

    with password hashing

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
+const bcrypt=require("bcrypt");
 router.use(bodyParser.json())
 router.use(bodyParser.urlencoded({ extended: true }));
 
@@ -10,8 +11,13 @@ exports.postuser= async (req,res,next)=>{
         const username = req.body.username;
         const useremail = req.body.useremail;
         const userpassword = req.body.userpassword;
-        const data= await User.create({username:username, useremail:useremail, userpassword:userpassword})
-        res.status(201).json({newUserDetail:data})
+        const saltrounds=10;
+        bcrypt.hash(userpassword,saltrounds,async(err,hash)=>{
+            console.log(err);
+            const data= await User.create({username:username, useremail:useremail, userpassword:hash})
+            res.status(201).json({newUserDetail:data})
+        })
+        
     }
     catch(err){
         res.status(500).json({
@@ -32,12 +38,18 @@ exports.loginUser=async(req,res,next)=>{
        const user=await User.findAll({where :{ useremail}})
        {
         if(user.length>0){
-            if(user[0].userpassword==userpassword){
-                res.status(200).json({success:true,message:"user logged in"});
-            }
-            else{
-                res.status(400).json({success:false,message:"password is wrogn"});
-            }
+            bcrypt.compare(userpassword,user[0].userpassword,(err,result)=>{
+                if(err){
+                    res.status(500).json({message:"something went wrogn",success:false})
+                }
+                if(!err){
+                    res.status(200).json({success:true,message:"user logged in"});
+                }
+                else{
+                    res.status(400).json({success:false,message:"password is wrogn"});
+                }
+            })
+        
         }
         else{
             res.status(404).json({success:false,message:"user not found"});
